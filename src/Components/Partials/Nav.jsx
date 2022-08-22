df2a772707e7d1768621bc74f536bf8dfc04f531
Gawmane <gawman.e@hotmail.com>
2022-08-22 13:16:46 +0200

    navigation + style

--- src/Components/Partials/Nav.jsx
+++ src/Components/Partials/Nav.jsx
@@ -1,10 +1,11 @@
 import style from '../../assets/Style/Nav.module.scss'
 import { NavLink } from "react-router-dom"
-
+import logo from "../../assets/Images/header-bg.png"
 
 export const Navigation = () => {
     return (
         <nav>
+            <img src={logo} alt="logo" />
             <ul>
                 <li><NavLink to={'/'}>Forside</NavLink></li>
                 <li><NavLink to={'/handelsbetingelser'}>Salgs- og handelsbetingelser</NavLink></li>
