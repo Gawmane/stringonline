7b5ee67eb8e711aae2d0cf8aec06eda1a7762745
Gawmane <gawman.e@hotmail.com>
2022-08-24 12:38:05 +0200

    Brands -produkter

--- src/Components/Pages/Brands/Brands.jsx
+++ src/Components/Pages/Brands/Brands.jsx
@@ -1,15 +1,18 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, Outlet, useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 
 import appService from "../../Tools/Appservice/AppService"
+import { SubNav } from "../../Partials/SubNav";
 
 
 export const Brands = () => {
 
 
     return (
-
-        <></>
+        <section>
+            <SubNav />
+            <Outlet />
+        </section>
 
     )
 }
@@ -23,7 +26,7 @@ export const BrandDetails = () => {
             try {
                 const result = await appService.getDetails('brands', id);
                 if (result.data) {
-                    setBrandDetails(result.data.items);
+                    setBrandDetails(result.data.item);
                 }
             } catch (error) {
                 console.log(error)
@@ -32,15 +35,53 @@ export const BrandDetails = () => {
         getBrands();
     }, [id]);
     return (
+        <>
+
+            <figure key={brandDetails.id}>
+                <img src={brandDetails.image_fullpath} alt={brandDetails.title} />
+                <figcaption>
+                    <h2>{brandDetails.title}</h2>
+                    <p>{brandDetails.description}</p>
+                </figcaption>
+            </figure>
+            {/* {brandDetails && brandDetails.map((brands) => {
+                return (
+                    <figure key={brands.id}>
+                        <Link to={''}> {brands.title} </Link>
 
 
-        <figure>
-            <img src={brandDetails.image_fullpath} alt={brandDetails.title} />
-            <figcaption>
-                <h2>{brandDetails.title}</h2>
-                <p>{brandDetails.description}</p>
-            </figcaption>
-        </figure>
+                        {brandDetails && brandDetails.products.map((product, i) => {
+                            return (
+                                <figcaption key={product.id}>
+                                    <h1>{product.name}</h1>
+                                    <figcaption> <img src="" alt="" /></figcaption>
+                                    <article>
 
+                                        <h2>{product.brand}</h2>
+                                        <p>{product.description_long}</p>
+                                        <a href=""></a>
+                                    </article>
+                                    <article>
+                                        <p>{product.price}</p>
+                                        <button></button>
+                                        <p>{product.stock}</p>
+                                    </article>
+                                    {/* <Link to={`/produkter/${subGroups.id}`}>{subGroups.title}</Link> 
+                                </figcaption>
+
+                            )
+                        })}
+
+
+
+
+                    </figure>
+
+
+
+
+                )
+            })}*/}
+        </>
     )
 }
