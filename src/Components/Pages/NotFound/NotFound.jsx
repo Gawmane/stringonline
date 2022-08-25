ec7a4d10c2beaa16eb4163cf0f6b7b91c40b2f6c
Gawmane <gawman.e@hotmail.com>
2022-08-25 10:04:34 +0200

    style

--- src/Components/Pages/NotFound/NotFound.jsx
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -1,11 +1,11 @@
-import style from '../../../assets/Style/App.scss';
+
 import { Layout } from '../../Tools/Layout/Layout';
 export const NotFound = () => {
     return (
-        <section className={style.notfound}>
+        <section style={{ textAlign: "center" }}>
             <Layout title="Fejl" description="Fejlside" />
-            <p>Det ser ud til vi ikke kunne finde den side du søgte</p>
-            <p>Prøv igen og jekt om du har skrevet rigtigt</p>
+            <p style={{ color: "white" }}>Det ser ud til vi ikke kunne finde den side du søgte</p>
+            <p style={{ color: "white" }}> Prøv igen og jekt om du har skrevet rigtigt</p>
             <img style={{ width: '40%' }} src="https://cdn.pixabay.com/photo/2019/02/04/08/38/pixel-cells-3974187_1280.png" alt="" />
 
         </section>
