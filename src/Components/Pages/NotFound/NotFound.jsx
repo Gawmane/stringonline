ce24984658fe9250c45053aa5ae44872903504b6
Gawmane <gawman.e@hotmail.com>
2022-08-25 09:57:44 +0200

    error side

--- src/Components/Pages/NotFound/NotFound.jsx
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -1,13 +1,13 @@
-import '../../../assets/Style/App.scss';
-import { SubNav } from '../../Partials/SubNav';
+import style from '../../../assets/Style/App.scss';
+import { Layout } from '../../Tools/Layout/Layout';
 export const NotFound = () => {
     return (
-        <>
-            <h1 >Fejl</h1>
+        <section className={style.notfound}>
+            <Layout title="Fejl" description="Fejlside" />
             <p>Det ser ud til vi ikke kunne finde den side du søgte</p>
             <p>Prøv igen og jekt om du har skrevet rigtigt</p>
             <img style={{ width: '40%' }} src="https://cdn.pixabay.com/photo/2019/02/04/08/38/pixel-cells-3974187_1280.png" alt="" />
-            <SubNav />
-        </>
+
+        </section>
     )
 }
