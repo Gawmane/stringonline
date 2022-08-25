321d12e9458934046d612452088598830475c42e
Gawmane <gawman.e@hotmail.com>
2022-08-25 13:56:59 +0200

    footer m style

--- src/Components/Partials/Footer.jsx
+++ src/Components/Partials/Footer.jsx
@@ -1,9 +1,11 @@
 import style from '../../assets/Style/Footer.module.scss'
+import footer from "../../assets/Images/footer.png"
+
 
 export const Footer = () => {
     return (
         <footer>
-            <p></p>
+            <img src={footer} alt="footerimage" />
         </footer>
     )
 }
